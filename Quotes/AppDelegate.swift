9d98d87a8b191e882f54bff4d2f38f24ca1e539b
Tianqi Guo <guotianqi1987@gmail.com>
2018-09-09 22:52:06 +0800

    Initial Commit

---
+++ Quotes/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Quotes
+//
+//  Created by 郭天祈 on 2018/9/9.
+//  Copyright © 2018年 郭天祈. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Quotes/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by 郭天祈 on 2018/9/9.
+//  Copyright © 2018年 郭天祈. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
