430de1befaf2aae4805733686e600dfb022034aa
Tianqi Guo <guotianqi1987@gmail.com>
2018-09-10 22:06:54 +0800

    Initial Commit

---
+++ BabyScript/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BabyScript
+//
+//  Created by 郭天祈 on 2018/9/10.
+//  Copyright © 2018年 郭天祈. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BabyScript/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BabyScript
+//
+//  Created by 郭天祈 on 2018/9/10.
+//  Copyright © 2018年 郭天祈. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
